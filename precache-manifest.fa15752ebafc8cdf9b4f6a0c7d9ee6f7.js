bc079cf38584b480e9c45e5a00a5cd2d9737cb26
Dmytro Anikin <dimianni@users.noreply.github.com>
2020-05-17 14:45:25 +0200

    Updates

---
+++ precache-manifest.fa15752ebafc8cdf9b4f6a0c7d9ee6f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "608a41cecc0bd91abec69adfd7fcf736",
+    "url": "/covid-19-stats/index.html"
+  },
+  {
+    "revision": "e1b182225f544d0df6a9",
+    "url": "/covid-19-stats/static/css/main.810ed484.chunk.css"
+  },
+  {
+    "revision": "ea2d1989488a897146c0",
+    "url": "/covid-19-stats/static/js/2.4b3dfb87.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/covid-19-stats/static/js/2.4b3dfb87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1b182225f544d0df6a9",
+    "url": "/covid-19-stats/static/js/main.8770d278.chunk.js"
+  },
+  {
+    "revision": "9d2c3011edf39237e2c1",
+    "url": "/covid-19-stats/static/js/runtime-main.7cea885c.js"
+  },
+  {
+    "revision": "aac117dca4188a7302157780bdde0c7c",
+    "url": "/covid-19-stats/static/media/Dimianni.aac117dc.svg"
+  }
+]);
